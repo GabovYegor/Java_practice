6ee1e4482edf31bfa42da02d974190ff43e7446d
egor <gsa74@yandex.ru>
2019-07-03 11:02:39 +0300

    add packeges struct

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import DataClasses.Example;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        Example ex = new Example();
+        ex.write();
     }
 }
