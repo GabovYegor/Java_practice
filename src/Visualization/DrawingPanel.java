c303d6ea7c2db329c0222e6ce4ebcc4b4c5c520b
egor <gsa74@yandex.ru>
2019-07-04 09:30:45 +0300

    refactoring

--- src/Visualization/DrawingPanel.java
+++ src/Visualization/DrawingPanel.java
@@ -14,6 +14,9 @@ import java.awt.event.MouseMotionListener;
 import java.util.Vector;
 
 class DrawingPanel extends JPanel {
+
+    private static final int RADIUS = 30;
+
     Vector points = new Vector();
     DrawingPanel(Vector new_odj){
         points = new_odj;
@@ -21,7 +24,10 @@ class DrawingPanel extends JPanel {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                points.addElement(new Example(e.getX(), e.getY(), 50, 50));
+                if(SwingUtilities.isLeftMouseButton(e))
+                    points.addElement(new Example(e.getX() - RADIUS/2, e.getY()-RADIUS/2, RADIUS, RADIUS));
+                else
+                    JOptionPane.showMessageDialog(null, "it_s right mouse button");
             }
         });
     }
@@ -36,18 +42,24 @@ class DrawingPanel extends JPanel {
             g2.fillOval(point.x, point.y, point.width, point.height);
         }
 
-        g2.setColor(Color.black);
-        g2.setStroke(new BasicStroke(8.0f));  // толщина равна 10
-        g2.drawLine(0, 0, 1000, 1000);
-        g2.setColor(Color.white);
-        g2.setStroke(new BasicStroke(2.0f));  // толщина равна 10
-        g2.drawLine(0, 0, 1000, 1000);
+        drawLine(g2, new Point(0, 0), new Point(1000, 1000));
+        drawLine(g2, new Point(0, 500), new Point(500, 0));
+    }
 
+    private void drawBlackLine(Graphics2D g2, Point from, Point to){
         g2.setColor(Color.black);
         g2.setStroke(new BasicStroke(8.0f));  // толщина равна 10
-        g2.drawLine(0, 500, 500, 0);
+        g2.drawLine(from.x, from.y, to.x, to.y);
+    }
+
+    private void drawWhiteLine(Graphics2D g2, Point from, Point to){
         g2.setColor(Color.white);
         g2.setStroke(new BasicStroke(2.0f));  // толщина равна 10
-        g2.drawLine(0, 500, 500, 0);
+        g2.drawLine(from.x, from.y, to.x, to.y);
+    }
+
+    private void drawLine(Graphics2D g2, Point from, Point to){
+        drawBlackLine(g2, from, to);
+        drawWhiteLine(g2, from, to);
     }
 }
